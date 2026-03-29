b91365000c7c784df4c0d353905a5772484ec8d0
Briza5 <BrizaL555@gmail.com>
2026-03-29 15:18:54 +0200

    feat: add email notification on form submission (L395)
    - configure Gmail SMTP backend in settings.py
    - load EMAIL_HOST_PASSWORD from environment variable via os.getenv()
    - send confirmation EmailMessage to applicant after successful DB save

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -16,6 +17,13 @@ def index(request):
 
             Form.objects.create(first_name=first_name, last_name=last_name, email=email,
                                 date=date, occupation=occupation)
+
+            message_body = f"Received application from \n {first_name}\n {last_name}, email: {email}, date: {date}, occupation: {occupation}"
+            email_message = EmailMessage(
+                "New Job Application",
+                message_body, to=[email])
+            email_message.send()
+
             messages.success(request,
                              f"Received application from {first_name} {last_name}, email: {email}, date: {date}, occupation: {occupation}")
     return render(request, 'index.html')
